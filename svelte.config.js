0b0f2369f505c16f9e4296b379f01259f394d039
Korbin Deary <korbin.deary45@gmail.com>
2025-10-27 10:04:24 -0400

    GH pages ready

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			// 👇 Only set this for production builds (GitHub Pages)
+			base: dev ? '' : '/csg'
+		},
+	}
+};
 
 export default config;
